bfb173d6bca8d635b1aad712d84b1b3480acdc6c
Yaneli Cruz <yanelicruz092@gmail.com>
2025-09-29 11:47:20 -0700

    completed react 2

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,33 +1,21 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
     });
-    
+
     setCharacters(updated);
+  }
+
+  function updateList(person) {
+    setCharacters([...characters, person]);
   }
 
   return (
@@ -35,6 +23,9 @@ function MyApp() {
       <Table
         characterData={characters}
         removeCharacter={removeOneCharacter}
+      />
+      <Form 
+        handleSubmit={updateList} 
       />
     </div>
   );
--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -6,7 +6,7 @@ function TableHeader() {
       <tr>
         <th>Name</th>
         <th>Job</th>
-        <th>Delete Button</th>
+        <th>Action</th>
       </tr>
     </thead>
   );
@@ -15,15 +15,13 @@ function TableHeader() {
 function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
-    <tr key={index}>
-      <td>{row.name}</td>
-      <td>{row.job}</td>
-      <td>
-        <button onClick={() => props.removeCharacter(index)}>
-          Delete
-        </button>
-      </td>
-    </tr>
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
+      </tr>
     );
   });
 

f631610612db3d38d6154465f0aa3f4895ea46aa
Yaneli Cruz <yanelicruz092@gmail.com>
2025-10-06 01:34:43 -0700

    added user function with boomerang

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -4,6 +4,11 @@ import express from "express";
 const app = express();
 const port = 8000;
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -15,35 +20,33 @@ const users = {
     {
       id: "xyz789",
       name: "Charlie",
-      job: "Janitor"
+      job: "Janitor",
     },
     {
       id: "abc123",
       name: "Mac",
-      job: "Bouncer"
+      job: "Bouncer",
     },
     {
       id: "ppp222",
       name: "Mac",
-      job: "Professor"
+      job: "Professor",
     },
     {
       id: "yat999",
       name: "Dee",
-      job: "Aspring actress"
+      job: "Aspring actress",
     },
     {
       id: "zap555",
       name: "Dennis",
-      job: "Bartender"
-    }
-  ]
+      job: "Bartender",
+    },
+  ],
 };
 
 const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
+  return users["users_list"].filter((user) => user["name"] === name);
 };
 
 app.get("/users", (req, res) => {
@@ -57,12 +60,30 @@ app.get("/users", (req, res) => {
   }
 });
 
-app.get("/users", (req, res) => {
-  res.send(users);
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
 });
 
 app.listen(port, () => {
-  console.log(
-    `Example app listening at http://localhost:${port}`
-  );
+  console.log(`Example app listening at http://localhost:${port}`);
 });
